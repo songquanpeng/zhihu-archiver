c243ee37c7f30077c8ec8af8f3c7b1eff6d99935
JustSong <songquanpeng@foxmail.com>
2021-12-22 17:22:30 +0800

    able to work now

--- README.md
+++ README.md
@@ -1,45 +1,4 @@
-## Abstract
-<p>
-  <img src="https://github-readme-stats.vercel.app/api?username=songquanpeng&show_icons=true&hide_border=true" alt="JustSong's Github Stats" width="58%" />
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=songquanpeng&layout=compact&hide_border=true&langs_count=10" alt="JustSong's Top Langs" width="37%" /> 
-</p>
+# 知乎热榜备份
 
-<p>
-  <img src="https://stats.justsong.cn/api/leetcode/?username=quanpeng&theme=light" alt="JustSong's LeetCode Stats" width="49%" />
-  <img src="https://stats.justsong.cn/api/zhihu/?username=songwonderful&theme=light" alt="JustSong's Zhihu Stats" width="49%" /> 
-</p>
-
-*Cards provided by [https://github.com/songquanpeng/stats-cards](https://github.com/songquanpeng/stats-cards).*
-
-
-## Recent Repos
-|Repo|Description|Last Update|
-|:--|:--|:--|
-|[songquanpeng](https://github.com/songquanpeng/songquanpeng)|Automatic update your GitHub readme profile with Github Actions.|`2021-12-19 13:51:13`|
-|[stats-cards](https://github.com/songquanpeng/stats-cards)|在 README 中展示你在知乎，B 站，LeetCode，掘金，CSDN，牛客等网站的数据，服务部署在 Vercel 上，保证服务稳定。Show your LeetCode stats in Github Profile.|`2021-12-19 00:22:14`|
-|[rest-reminder](https://github.com/songquanpeng/rest-reminder)|A reminder that will notify you when you need a rest.|`2021-12-11 10:44:25`|
-|[pytorch-deployment](https://github.com/songquanpeng/pytorch-deployment)|A template for rapid deployment of PyTorch models.|`2021-12-08 16:08:38`|
-|[pytorch-template](https://github.com/songquanpeng/pytorch-template)|A template for PyTorch.|`2021-12-06 11:15:36`|
-|[go-file](https://github.com/songquanpeng/go-file)|基于 Go 的文件分享工具，仅单可执行文件，开箱即用. File sharing tool written in Go.|`2021-12-05 13:02:42`|
-|[scripts](https://github.com/songquanpeng/scripts)|在终端执行的脚本，浏览器脚本所在的仓库：https://github.com/songquanpeng/userscripts|`2021-11-29 21:14:41`|
-|[filesystem-emulator](https://github.com/songquanpeng/filesystem-emulator)|Linux filesystem emulator.|`2021-11-24 15:05:12`|
-|[handwriting-generator](https://github.com/songquanpeng/handwriting-generator)|手写中文文章生成，可用于生成各种需要手写的文件|`2021-11-22 20:21:49`|
-|[blog](https://github.com/songquanpeng/blog)|基于 Node.js 的个人博客系统|`2021-09-23 23:26:32`|
-
-## Top Repos
-|Repo|Description|Star|
-|:--|:--|:--|
-|[message-pusher](https://github.com/songquanpeng/message-pusher)|搭建专属于你的微信消息推送服务，支持 Markdown，支持发送邮件消息，可以选择部署在 Heroku 上，无需自己的服务器。|`413`|
-|[go-file](https://github.com/songquanpeng/go-file)|基于 Go 的文件分享工具，仅单可执行文件，开箱即用. File sharing tool written in Go.|`49`|
-|[stats-cards](https://github.com/songquanpeng/stats-cards)|在 README 中展示你在知乎，B 站，LeetCode，掘金，CSDN，牛客等网站的数据，服务部署在 Vercel 上，保证服务稳定。Show your LeetCode stats in Github Profile.|`32`|
-|[blog](https://github.com/songquanpeng/blog)|基于 Node.js 的个人博客系统|`24`|
-|[battle-city](https://github.com/songquanpeng/battle-city)|基于 TypeScript 的《坦克大战》的非标准实现。Yet another Battle City implemented in TypeScript.|`17`|
-|[v2ex-style-forum](https://github.com/songquanpeng/v2ex-style-forum)|V2ex 风格的论坛程序. V2ex's Node.js clone.|`15`|
-|[lan-share](https://github.com/songquanpeng/lan-share)|File sharing tool. |`12`|
-|[js-interpreter](https://github.com/songquanpeng/js-interpreter)|一个简陋的 JavaScript 解释器实现。A simple JavaScript interpreter, build for beginners.|`11`|
-|[hexo-theme-lightx](https://github.com/songquanpeng/hexo-theme-lightx)|Hexo theme lightx.|`8`|
-|[remote-executor](https://github.com/songquanpeng/remote-executor)|Execute predefined commands on your server when receiving a specific HTTP GET request.|`8`|
-
-
-
-*Last automatic update at 2021-12-19 20:28:36 by [https://github.com/songquanpeng/songquanpeng/blob/master/update.py](https://github.com/songquanpeng/songquanpeng/blob/master/update.py).*
+## 描述
+每日自动备份知乎热榜，并构建静态网站。
--- update.py
+++ update.py
@@ -1,149 +1,124 @@
-import requests
-import os
 import datetime
-from dateutil import tz
+import os
+from shutil import copyfile
 
-token = ''
-current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-top_repo_num = 10
-recent_repo_num = 10
+import requests
+from bs4 import BeautifulSoup
 
-from_zone = tz.tzutc()
-to_zone = tz.tzlocal()
+base_path = "docs"
 
 
-def fetcher(username: str):
-    result = {
-        'name': '',
-        'public_repos': 0,
-        'top_repos': [],
-        'recent_repos': []
-    }
-    user_info_url = "https://api.github.com/users/{}".format(username)
-    header = {} if token == "" else {"Authorization": "bearer {}".format(token)}
-    res = requests.get(user_info_url, header)
-    user = res.json()
-    result['name'] = user['name']
-    repos = []
-    for i in range(1, 2 + user['public_repos'] // 100):
-        all_repos_url = "https://api.github.com/users/{}/repos?per_page=100&page={}".format(username, i)
-        res = requests.get(all_repos_url, header)
-        repos.extend(res.json())
-    processed_repos = []
-    for repo in repos:
-        if repo['fork']:
-            continue
-        processed_repo = {
-            'score': repo['stargazers_count'] + repo['watchers_count'] + repo['forks_count'],
-            'star': repo['stargazers_count'],
-            'link': repo['html_url'],
-            'created_at': repo['created_at'],
-            'updated_at': repo['updated_at'],
-            'pushed_at': repo['pushed_at'],
-            'name': repo['name'],
-            'description': repo['description']
-        }
-        date = datetime.datetime.strptime(processed_repo['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
-        date = date.replace(tzinfo=from_zone)
-        date = date.astimezone(to_zone)
-        processed_repo['pushed_at'] = date.strftime('%Y-%m-%d %H:%M:%S')
-        processed_repos.append(processed_repo)
-    top_repos = sorted(processed_repos, key=lambda x: x['score'], reverse=True)
-    top_repos = top_repos[:top_repo_num]
-    result['top_repos'] = top_repos
-    recent_repos = sorted(processed_repos, key=lambda x: x['pushed_at'], reverse=True)
-    recent_repos = recent_repos[:recent_repo_num]
-    result['recent_repos'] = recent_repos
-    return result
+def fetcher():
+    res = requests.get("https://www.zhihu.com/hot", headers={
+        "authority": "www.zhihu.com",
+        "user-agent": "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+    })
+    if res.status_code != 200:
+        print("Failed to fetch html.")
+        exit(-1)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    main_tag = soup.findAll('main')[0]
+    list_tag = None
+    for child in main_tag.children:
+        if child.name == 'div':
+            list_tag = child
+            break
+    if list_tag is None:
+        print("Failed to find list tag.")
+        exit(-1)
+    results = []
+    for item in list_tag.children:
+        try:
+            if item.name != "a":
+                continue
+            link = item.attrs['href']
+            texts = []
+            is_second_div = False
+            for tag in item.children:
+                if tag.name == 'div':
+                    if is_second_div:
+                        for sub_tag in tag.children:
+                            if sub_tag.name in ['div', 'h1']:
+                                texts.append(sub_tag.text)
+                    else:
+                        is_second_div = True
+            if len(texts) == 2:
+                result = {
+                    "link": link,
+                    "title": texts[0],
+                    "description": "",
+                    "hot": texts[1]
+                }
+            else:
+                result = {
+                    "link": link,
+                    "title": texts[0],
+                    "description": texts[1],
+                    "hot": texts[2]
+                }
+        except Exception as e:
+            result = {
+                "link": "",
+                "title": "Error",
+                "description": str(e),
+                "hot": ""
+            }
+            print(e)
+        results.append(result)
+    return results
 
 
-abstract_tpl = """## Abstract
-<p>
-  <img src="https://github-readme-stats.vercel.app/api?username={github_username}&show_icons=true&hide_border=true" alt="{github_name}'s Github Stats" width="58%" />
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username={github_username}&layout=compact&hide_border=true&langs_count=10" alt="{github_name}'s Top Langs" width="37%" /> 
-</p>
-
-<p>
-  <img src="https://stats.justsong.cn/api/leetcode/?username=quanpeng&theme=light" alt="JustSong's LeetCode Stats" width="49%" />
-  <img src="https://stats.justsong.cn/api/zhihu/?username=songwonderful&theme=light" alt="JustSong's Zhihu Stats" width="49%" /> 
-</p>
-
-*Cards provided by [https://github.com/songquanpeng/stats-cards](https://github.com/songquanpeng/stats-cards).*
-
-"""
-
-zhihu_tpl = "[![{github_name}'s Zhihu Stats](https://stats.justsong.cn/api/zhihu?username={zhihu_username})](https://github.com/songquanpeng/readme-stats)\n"
-
-recent_repos_tpl = """\n## Recent Repos
-|Repo|Description|Last Update|
-|:--|:--|:--|
-"""
-
-top_repos_tpl = """\n## Top Repos
-|Repo|Description|Star|
-|:--|:--|:--|
-""".format(current_time)
-
-footer_tpl = """
-\n
-*Last automatic update at {} by [https://github.com/songquanpeng/songquanpeng/blob/master/update.py](https://github.com/songquanpeng/songquanpeng/blob/master/update.py).*
-""".format(current_time)
+def write_content(data, title, time, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(f"# {title}\n\n")
+        f.write(f"抓取于：`{time}`\n\n")
+        for i, item in enumerate(data):
+            link = item['link']
+            title = item['title']
+            description = item['description']
+            hot = item['hot']
+            f.write(f"### {i + 1}. {title}\n")
+            f.write(f"{hot} 链接：[{link}]({link})\n\n")
+            f.write(f"{description}\n\n")
 
 
-def render(github_username, github_data, zhihu_username='') -> str:
-    markdown = ""
-    global abstract_tpl
-    if zhihu_username:
-        abstract_tpl += zhihu_tpl
-    markdown += abstract_tpl.format(github_username=github_username, github_name=github_data['name'],
-                                    zhihu_username=zhihu_username)
-    global recent_repos_tpl
-    for repo in github_data['recent_repos']:
-        recent_repos_tpl += "|[{name}]({link})|{description}|`{pushed_at}`|\n".format(**repo)
-    markdown += recent_repos_tpl
-    global top_repos_tpl
-    for repo in github_data['top_repos']:
-        top_repos_tpl += "|[{name}]({link})|{description}|`{star}`|\n".format(**repo)
-    markdown += top_repos_tpl
-    markdown += footer_tpl
-    return markdown
-
-
-def writer(markdown) -> bool:
-    ok = True
-    try:
-        with open('./README.md', 'w') as f:
-            f.write(markdown)
-    except IOError:
-        ok = False
-        print('unable to write to file')
-    return ok
-
-
-def pusher():
-    commit_message = ":pencil2: update on {}".format(current_time)
-    os.system('git add ./README.md')
-    if os.getenv('DEBUG'):
-        return
-    os.system('git commit -m "{}"'.format(commit_message))
-    os.system('git push')
+def build_toc():
+    with open(os.path.join(base_path, 'toc.md'), 'w', encoding='utf-8') as f:
+        f.write(f"* [介绍](/)\n")
+        paths = os.listdir(base_path)
+        paths.sort()
+        for path in paths:
+            full_path = os.path.join(base_path, path)
+            if not os.path.isdir(full_path):
+                continue
+            f.write(f"* [{path}]({path}/)\n")
+            sub_files = os.listdir(full_path)
+            sub_files.sort()
+            for file in sub_files:
+                if file == 'README.md':
+                    continue
+                f.write(f"  * [{file}]({path}/{file})\n")
 
 
 def main():
-    global top_repo_num
-    global recent_repo_num
-    top_repo_num = 10
-    recent_repo_num = 10
-    github_username = os.getenv('GITHUB_USERNAME')
-    if not github_username:
-        cwd = os.getcwd()
-        github_username = os.path.split(cwd)[-1]
-    zhihu_username = os.getenv('ZHIHU_USERNAME')
-    github_data = fetcher(github_username)
-    markdown = render(github_username, github_data, zhihu_username)
-    if writer(markdown):
-        pass
-        # pusher()
+    data = fetcher()
+    time = datetime.datetime.now()
+    year, month, day = time.year, time.month, time.day
+    time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    chapter_str = f"{year}-{month}"
+    page_str = f"{day:02}.md"
+    title = f"{year} 年 {month} 月 {day} 日的知乎热榜存档"
+    if not os.path.exists(os.path.join(base_path, chapter_str)):
+        os.mkdir(os.path.join(base_path, chapter_str))
+        with open(os.path.join(base_path, chapter_str, 'README.md'), 'w', encoding='utf-8') as f:
+            f.write(f"# {year} 年 {month} 月")
+
+    file_path = os.path.join(base_path, chapter_str, page_str)
+    write_content(data, title, time_str, file_path)
+    copyfile("./README.md", "./docs/README.md")
+    build_toc()
 
 
 if __name__ == '__main__':
